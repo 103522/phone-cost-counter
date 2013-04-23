1e7aea8f0a78156e79be7111064c388f8aaf8903
Zhi <yfwz100@yeah.net>
2013-04-23 16:43:38 +0800

    修订图形界面部分以及接口。  - 新增一个对话框。  - 以及抽象出PhoneCostStrategy类。

---
+++ src/dsttime/PhoneCostStrategy.java
@@ -0,0 +1,17 @@
+package dsttime;
+
+/**
+ * A strategy interface for phone cost counter.
+ * 
+ * @author yfwz100
+ */
+public interface PhoneCostStrategy {
+    
+    /**
+     * Get the cos according to the minutes.
+     * 
+     * @param minutes the minutes to calculate the cost.
+     * @return the money.
+     */
+    double getCost(long minutes);
+}
