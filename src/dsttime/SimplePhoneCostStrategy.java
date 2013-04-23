da26c320bdbb163e6ad9ac069eb0100a049e87b5
robinhood <robinhood@robinhood-PC>
2013-04-23 17:05:55 +0800

    创建SimplePhoneCostStrategy

---
+++ src/dsttime/SimplePhoneCostStrategy.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsttime;
+
+/**
+ *
+ * @author robinhood
+ */
+public class SimplePhoneCostStrategy implements PhoneCostStrategy {
+
+    @Override
+    public double getCost(long minutes) {
+        
+        return 0;
+    }
+    
+}
